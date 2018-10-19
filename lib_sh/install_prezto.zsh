e62c2a36581b21e736e7694c21a8759bdf0596b3
Neoptolemos Kyriakou <neostixz@gmail.com>
2018-10-19 18:14:53 +0300

    fixed prezto installation

--- lib_sh/install_prezto.zsh
+++ lib_sh/install_prezto.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/prezto/runcoms/^README.md(.N); do
-  (echo -n "  linking $rcfile  " && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}")
+for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/prezto/runcoms/^README.md(.N); do
+  (echo -n "  linking $rcfile  " && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" && echo)
 done
 
 exit 0
